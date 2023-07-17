f16a4ee105099fcc8b5ccd5546376615b8f46b0f
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-07-17 10:23:58 +0700

    fixed bugs

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const StickyResumeDownload = () => {
   const handleDownload = () => {
-    window.location.href = "src/images/Benjie Angeles Resume.pdf";
+    const fileUrl = "public/assets/Benjie-Angeles-Resume.pdf";
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = "Benjie-Angeles-Resume.pdf";
+    link.click();
   };
 
   return (
-    <div className="fixed top-0 z-0 p-4">
+    <div className="fixed top-0 left-0 p-4">
       <button
         className="bg-gray-900 hover:bg-gray-300 dark:bg-white dark:text-black dark:hover:bg-gray-500 text-white font-bold py-1 px-2 sm:py-2 sm:px-4 rounded-full text-xs sm:text-sm"
         onClick={handleDownload}
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default [
   {
     title: "Music App",
-    imgUrl: "src/images/music-app.jpg",
+    imgUrl: "assets/music-app.jpg",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://thebenjieangeles.github.io/music-app/",
     code: "https://github.com/thebenjieangeles/music-app",
   },
   {
     title: "Calculator App",
-    imgUrl: "src/images/calculator-app.jpg",
+    imgUrl: "assets/calculator-app.jpg",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://thebenjieangeles.github.io/simple-calculator-with-JS/",
     code: "https://github.com/thebenjieangeles/simple-calculator-with-JS",
   },
   {
     title: "Developer Portfolio",
-    imgUrl: "src/images/developer.jpg",
+    imgUrl: "assets/developer.jpg",
     stack: ["React", "Tailwind.css", "Vite"],
     link: "https://thebenjieangeles.github.io/simple-calculator-with-JS/",
     code: "https://github.com/thebenjieangeles/music-app",
